eeba7924409690a3ec097230aafe129d3b8094a0
Hygor Cesconetto <cesconetts@gmail.com>
2024-03-11 04:38:02 -0300

    feat: Add API-itens(weapon,armour,trinket)

---
+++ back/controllers.py
@@ -0,0 +1,117 @@
+from app import app
+from flask import jsonify, request
+from db import Weapom,Armour,Trinket, SyntaxException ,NotFoundException
+
+
+
+
+##-------------------------------------------------EXCEPTIONS
+@app.errorhandler(SyntaxException)
+def syntax_exception(error):
+    return 'Sintaxe incorreta, tente novamente.', 400
+
+@app.errorhandler(NotFoundException)
+def syntax_exception(error):
+    return 'Item não encontrado.', 404
+
+
+## CRUD --------------------------------------------WEAPON
+#READ
+@app.get("/weapon")
+def weapon_getAll():
+    return jsonify(Weapom().getAll())
+
+@app.get("/weapon/<int:id>")
+def weapon_getByID(id):
+    return jsonify(Weapom().getByID(id))
+
+@app.get("/weapon/<prop>")
+def weapon_getByType(prop):
+    return jsonify(Weapom().getByProp(prop))
+
+#CREATE
+@app.post("/weapon")
+def new_weapon():
+    Weapom().new(request.json)
+    return "ok"
+
+#DELETE
+@app.delete("/weapon/<int:id>")
+def del_weapon(id):
+    Weapom().drop(id)
+    return "ok"
+
+#UPDATE
+@app.route("/weapon", methods =["PUT","PATCH"])
+def update_weapon():
+    if request.method == "PUT": Weapom().full_update(request.json)
+    elif request.method == "PATCH": Weapom().parcial_update(request.json)
+    return "ok"
+
+
+## CRUD --------------------------------------------ARMOUR
+#READ
+@app.get("/armour")
+def armour_getAll():
+    return jsonify(Armour().getAll())
+
+@app.get("/armour/<int:id>")
+def armour_getByID(id):
+    return jsonify(Armour().getByID(id))
+
+@app.get("/armour/<prop>")
+def armour_getByType(prop):
+    return jsonify(Armour().getByProp(prop))
+
+#CREATE
+@app.post("/armour")
+def new_armour():
+    Armour().new(request.json)
+    return "ok"
+
+#DELETE
+@app.delete("/armour/<int:id>")
+def del_armour(id):
+    Armour().drop(id)
+    return "ok"
+
+#UPDATE
+@app.route("/armour", methods =["PUT","PATCH"])
+def update_armour():
+    if request.method == "PUT": Armour().full_update(request.json)
+    elif request.method == "PATCH": Armour().parcial_update(request.json)
+    return "ok"
+
+
+## CRUD --------------------------------------------TRINKET
+#READ
+@app.get("/trinket")
+def trinket_getAll():
+    return jsonify(Trinket().getAll())
+
+@app.get("/trinket/<int:id>")
+def trinket_getByID(id):
+    return jsonify(Trinket().getByID(id))
+
+@app.get("/trinket/<prop>")
+def trinket_getByType(prop):
+    return jsonify(Trinket().getByProp(prop))
+
+#CREATE
+@app.post("/trinket")
+def new_trinket():
+    Trinket().new(request.json)
+    return "ok"
+
+#DELETE
+@app.delete("/trinket/<int:id>")
+def del_trinket(id):
+    Trinket().drop(id)
+    return "ok"
+
+#UPDATE
+@app.route("/trinket", methods =["PUT","PATCH"])
+def update_trinket():
+    if request.method == "PUT": Trinket().full_update(request.json)
+    elif request.method == "PATCH": Trinket().parcial_update(request.json)
+    return "ok"
---
+++ back/db.py
@@ -0,0 +1,328 @@
+import mysql.connector as con
+
+class SyntaxException(Exception):pass
+class NotFoundException(Exception):pass
+
+
+db = con.connect(user="root",
+                 password="2591439",
+                 database="dedb")
+
+#---------------------------------------------------------------------------------------------WEAPON
+class Weapom():
+	def getAll(self):
+		try:
+			crs = db.cursor(dictionary=True)
+			query="""SELECT * FROM weapon;"""
+			crs.execute(query)
+			results = crs.fetchall()
+			crs.close()
+			return results
+		except: raise SyntaxException
+
+	def getByID(self, id:int):
+		try:
+			crs = db.cursor(dictionary=True)
+			crs.execute(f"""SELECT * FROM weapon where `id` = {id};""")
+			result = crs.fetchone()
+			crs.close()
+
+			if result == None : raise NotFoundException
+			return result
+		except NotFoundException: raise NotFoundException
+		except Exception: raise SyntaxException
+
+	def getByProp(self, prop:str):
+		try:
+			crs= db.cursor(dictionary=True)
+			crs.execute("select `type` from  weapon group by `type`;")
+			types=[x["type"] for x in crs.fetchall()]
+
+			if prop in types:
+				crs.execute(f"select * from  weapon WHERE `type`= '{prop.lower()}';")
+				results= crs.fetchall()
+				crs.close()
+			else: 
+				crs.execute(f"select * from  weapon WHERE `name`= '{prop.lower()}';")
+				results= crs.fetchone()
+				if results == None: raise NotFoundException
+				crs.close()
+			return results
+		except NotFoundException: raise NotFoundException
+		except Exception:raise SyntaxException
+
+	def new(self, data:dict):
+		try:
+			keys=""
+			values=""
+			for key in data.keys():
+				keys+="`%s`,"%key
+				match key:
+					case "name": values+="'%s',"%data[key].lower()
+					case "type": values+="'%s',"%data[key].lower()
+					case _: values+="%s,"%data[key]
+				
+			crs = db.cursor()
+			query="""INSERT INTO weapon(%s) VALUES(%s);""" %(keys[:-1],values[:-1])
+			crs.execute(query)
+			db.commit()
+			crs.close()
+		except Exception:raise SyntaxException
+	
+	def drop(self, id:int):
+		try:
+			Weapom().getByID(id)
+			crs = db.cursor()
+			crs.execute("""DELETE FROM weapon WHERE `id` = %s;"""%id)
+			db.commit()
+			crs.close()
+		except NotFoundException : raise NotFoundException
+		except Exception: raise SyntaxException
+		
+	def full_update(self, data:dict):
+		try:
+			Weapom().getByID(data["id"])
+			if len(data) != 6: raise SyntaxError
+			crs = db.cursor()
+			crs.execute(f"""UPDATE weapon SET 
+			   `type`='{data["type"].lower()}', 
+			   `name`='{data["name"].lower()}', 
+			   `atk`={data["atk"]},
+			   `atks`={data["atks"]},
+			   `def`={data["def"]}
+			   WHERE `id` = {data["id"]};""")
+			db.commit()
+			crs.close()
+		except NotFoundException: raise NotFoundException
+		except Exception:raise SyntaxException
+
+	def parcial_update(self, data:dict):
+		try:
+			Weapom().getByID(data["id"])
+			sets = ""
+			for key in data.keys():
+				sets+= f"`{key}`="
+				match key:
+					case "name": sets += f"'{data[key].lower()}',"
+					case "type": sets += f"'{data[key].lower()}',"
+					case _: sets += f"{data[key]},"
+
+			crs = db.cursor()
+			crs.execute(f"""UPDATE weapon SET {sets[:-1]} WHERE `id`={data["id"]} ;""")
+			db.commit()
+			crs.close()
+		except NotFoundException: raise NotFoundException
+		except Exception:raise SyntaxException
+
+
+#---------------------------------------------------------------------------------------------ARMOUR
+class Armour():
+	def getAll(self):
+		try:
+			crs = db.cursor(dictionary=True)
+			query="""SELECT * FROM armour;"""
+			crs.execute(query)
+			results = crs.fetchall()
+			crs.close()
+			return results
+		except: raise SyntaxException
+
+	def getByID(self, id:int):
+		try:
+			crs = db.cursor(dictionary=True)
+			crs.execute(f"""SELECT * FROM armour where `id` = {id};""")
+			result = crs.fetchone()
+			crs.close()
+
+			if result == None : raise NotFoundException
+			return result
+		except NotFoundException: raise NotFoundException
+		except Exception: raise SyntaxException
+
+	def getByProp(self, prop:str):
+		try:
+			crs= db.cursor(dictionary=True)
+			crs.execute("select `type` from  armour group by `type`;")
+			types=[x["type"] for x in crs.fetchall()]
+
+			if prop in types:
+				crs.execute(f"select * from  armour WHERE `type`= '{prop.lower()}';")
+				results= crs.fetchall()
+				crs.close()
+			else: 
+				crs.execute(f"select * from  armour WHERE `name`= '{prop.lower()}';")
+				results= crs.fetchone()
+				if results == None: raise NotFoundException
+				crs.close()
+			return results
+		except NotFoundException: raise NotFoundException
+		except Exception:raise SyntaxException
+
+	def new(self, data:dict):
+		try:
+			keys=""
+			values=""
+			for key in data.keys():
+				keys+="`%s`,"%key
+				match key:
+					case "name": values+="'%s',"%data[key].lower()
+					case "type": values+="'%s',"%data[key].lower()
+					case _: values+="%s,"%data[key]
+				
+			crs = db.cursor()
+			query="""INSERT INTO armour(%s) VALUES(%s);""" %(keys[:-1],values[:-1])
+			crs.execute(query)
+			db.commit()
+			crs.close()
+		except Exception:raise SyntaxException
+	
+	def drop(self, id:int):
+		try:
+			Weapom().getByID(id)
+			crs = db.cursor()
+			crs.execute("""DELETE FROM armour WHERE `id` = %s;"""%id)
+			db.commit()
+			crs.close()
+		except NotFoundException : raise NotFoundException
+		except Exception: raise SyntaxException
+		
+	def full_update(self, data:dict):
+		try:
+			Weapom().getByID(data["id"])
+			if len(data) != 6: raise SyntaxError
+			crs = db.cursor()
+			crs.execute(f"""UPDATE armour SET 
+			   `type`='{data["type"].lower()}', 
+			   `name`='{data["name"].lower()}', 
+			   `atk`={data["atk"]},
+			   `hp`={data["hp"]},
+			   `def`={data["def"]}
+			   WHERE `id` = {data["id"]};""")
+			db.commit()
+			crs.close()
+		except NotFoundException: raise NotFoundException
+		except Exception:raise SyntaxException
+
+	def parcial_update(self, data:dict):
+		try:
+			Weapom().getByID(data["id"])
+			sets = ""
+			for key in data.keys():
+				sets+= f"`{key}`="
+				match key:
+					case "name": sets += f"'{data[key].lower()}',"
+					case "type": sets += f"'{data[key].lower()}',"
+					case _: sets += f"{data[key]},"
+
+			crs = db.cursor()
+			crs.execute(f"""UPDATE armour SET {sets[:-1]} WHERE `id`={data["id"]} ;""")
+			db.commit()
+			crs.close()
+		except NotFoundException: raise NotFoundException
+		except Exception:raise SyntaxException
+
+#---------------------------------------------------------------------------------------------TRINKET
+class Trinket():
+	def getAll(self):
+		try:
+			crs = db.cursor(dictionary=True)
+			query="""SELECT * FROM trinket;"""
+			crs.execute(query)
+			results = crs.fetchall()
+			crs.close()
+			return results
+		except: raise SyntaxException
+
+	def getByID(self, id:int):
+		try:
+			crs = db.cursor(dictionary=True)
+			crs.execute(f"""SELECT * FROM trinket where `id` = {id};""")
+			result = crs.fetchone()
+			crs.close()
+
+			if result == None : raise NotFoundException
+			return result
+		except NotFoundException: raise NotFoundException
+		except Exception: raise SyntaxException
+
+	def getByProp(self, prop:str):
+		try:
+			crs= db.cursor(dictionary=True)
+			crs.execute("select `type` from  trinket group by `type`;")
+			types=[x["type"] for x in crs.fetchall()]
+
+			if prop in types:
+				crs.execute(f"select * from  trinket WHERE `type`= '{prop.lower()}';")
+				results= crs.fetchall()
+				crs.close()
+			else: 
+				crs.execute(f"select * from  trinket WHERE `name`= '{prop.lower()}';")
+				results= crs.fetchone()
+				if results == None: raise NotFoundException
+				crs.close()
+			return results
+		except NotFoundException: raise NotFoundException
+		except Exception:raise SyntaxException
+
+	def new(self, data:dict):
+		try:
+			keys=""
+			values=""
+			for key in data.keys():
+				keys+="`%s`,"%key
+				match key:
+					case "name": values+="'%s',"%data[key].lower()
+					case "type": values+="'%s',"%data[key].lower()
+					case _: values+="%s,"%data[key]
+				
+			crs = db.cursor()
+			query="""INSERT INTO trinket(%s) VALUES(%s);""" %(keys[:-1],values[:-1])
+			crs.execute(query)
+			db.commit()
+			crs.close()
+		except Exception:raise SyntaxException
+	
+	def drop(self, id:int):
+		try:
+			Weapom().getByID(id)
+			crs = db.cursor()
+			crs.execute("""DELETE FROM trinket WHERE `id` = %s;"""%id)
+			db.commit()
+			crs.close()
+		except NotFoundException : raise NotFoundException
+		except Exception: raise SyntaxException
+		
+	def full_update(self, data:dict):
+		try:
+			Weapom().getByID(data["id"])
+			if len(data) != 6: raise SyntaxError
+			crs = db.cursor()
+			crs.execute(f"""UPDATE trinket SET 
+			   `type`='{data["type"].lower()}', 
+			   `name`='{data["name"].lower()}', 
+			   `atk`={data["atk"]},
+			   `hp`={data["hp"]},
+			   `def`={data["def"]}
+			   WHERE `id` = {data["id"]};""")
+			db.commit()
+			crs.close()
+		except NotFoundException: raise NotFoundException
+		except Exception:raise SyntaxException
+
+	def parcial_update(self, data:dict):
+		try:
+			Weapom().getByID(data["id"])
+			sets = ""
+			for key in data.keys():
+				sets+= f"`{key}`="
+				match key:
+					case "name": sets += f"'{data[key].lower()}',"
+					case "type": sets += f"'{data[key].lower()}',"
+					case _: sets += f"{data[key]},"
+
+			crs = db.cursor()
+			crs.execute(f"""UPDATE trinket SET {sets[:-1]} WHERE `id`={data["id"]} ;""")
+			db.commit()
+			crs.close()
+		except NotFoundException: raise NotFoundException
+		except Exception:raise SyntaxException
