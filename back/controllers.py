60f5f880547afa1c1c3421606ef8ae3df3c01244
Hygor Cesconetto <hygor.cesconetto@aluno.faculdadeimpacta.com.br>
2024-05-03 12:57:06 -0300

    remove angular, adapt backend, change db

--- back/controllers.py
+++ back/controllers.py
@@ -1,6 +1,6 @@
 from app import app
 from flask import jsonify, request , render_template, redirect, url_for
-from db import Weapom,Armour,Trinket,Account,Build,     SyntaxException ,NotFoundException, UserAlreadyExistException
+from db import Weapom,Armour,Trinket,Account,Build, SyntaxException ,NotFoundException, UserAlreadyExistException
 
 
 @app.get("/")
@@ -21,115 +21,7 @@ def syntax_exception(error):
     return 'Não encontrado.', 404
 
 
-## CRUD --------------------------------------------WEAPON
-#READ
-@app.get("/weapon")
-def weapon_getAll():
-    data=Weapom().getAll()
-    return render_template("weapon_all.html",data=data)
 
-@app.get("/weapon/<int:id>")
-def weapon_getByID(id):
-    data=dict(Weapom().getByID(id))
-    return render_template("weapon_id.html", data = data)
-
-@app.get("/weapon/<prop>")
-def weapon_getByType(prop):
-    data = Weapom().getByProp(prop)
-    return render_template("weapon_prop.html", data=data)
-
-@app.get("/weapon_api")
-def weapon_getApi():
-    data=Weapom().getAll()
-    return jsonify(data)
-
-#CREATE
-@app.post("/weapon")
-def new_weapon():
-    Weapom().new(request.json)
-    return jsonify({"message":"ok"})
-
-#DELETE
-@app.delete("/weapon/<int:id>")
-def del_weapon(id):
-        Weapom().drop(id)
-        return ("ok",200)
-
-#UPDATE
-@app.route("/weapon", methods =["PUT","PATCH"])
-def update_weapon():
-    if request.method == "PUT": Weapom().full_update(request.json)
-    elif request.method == "PATCH": Weapom().parcial_update(request.json)
-    return "ok"
-
-
-## CRUD --------------------------------------------ARMOUR
-#READ
-@app.get("/armour")
-def armour_getAll():
-    return jsonify(Armour().getAll())
-
-@app.get("/armour/<int:id>")
-def armour_getByID(id):
-    return jsonify(Armour().getByID(id))
-
-@app.get("/armour/<prop>")
-def armour_getByType(prop):
-    return jsonify(Armour().getByProp(prop))
-
-#CREATE
-@app.post("/armour")
-def new_armour():
-    Armour().new(request.json)
-    return jsonify({"message":"ok"})
-
-
-#DELETE
-@app.delete("/armour/<int:id>")
-def del_armour(id):
-    Armour().drop(id)
-    return "ok"
-
-#UPDATE
-@app.route("/armour", methods =["PUT","PATCH"])
-def update_armour():
-    if request.method == "PUT": Armour().full_update(request.json)
-    elif request.method == "PATCH": Armour().parcial_update(request.json)
-    return "ok"
-
-
-## CRUD --------------------------------------------TRINKET
-#READ
-@app.get("/trinket")
-def trinket_getAll():
-    return jsonify(Trinket().getAll())
-
-@app.get("/trinket/<int:id>")
-def trinket_getByID(id):
-    return jsonify(Trinket().getByID(id))
-
-@app.get("/trinket/<prop>")
-def trinket_getByType(prop):
-    return jsonify(Trinket().getByProp(prop))
-
-#CREATE
-@app.post("/trinket")
-def new_trinket():
-    Trinket().new(request.json)
-    return jsonify({"message":"ok"})
-
-#DELETE
-@app.delete("/trinket/<int:id>")
-def del_trinket(id):
-    Trinket().drop(id)
-    return "ok"
-
-#UPDATE
-@app.route("/trinket", methods =["PUT","PATCH"])
-def update_trinket():
-    if request.method == "PUT": Trinket().full_update(request.json)
-    elif request.method == "PATCH": Trinket().parcial_update(request.json)
-    return "ok"
 
 
 ## CRUD -------------------------------------------- ACCOUNTS
@@ -138,10 +30,6 @@ def update_trinket():
 def account_getAll():
     data = Account().getAll()
     return render_template("account.html", data=data)
-
-@app.get("/account/<int:id>")
-def account_getByID(id):
-    return jsonify(Account().getByID(id))
 
 #CREATE
 @app.post("/account")
--- back/db.py
+++ back/db.py
@@ -3,388 +3,49 @@ import mysql.connector as con
 class SyntaxException(Exception):pass
 class NotFoundException(Exception):pass
 class UserAlreadyExistException(Exception):pass
+class DBConnectionFail(Exception):pass
 
 
 class DBCon ():
 	def up ():
 		try:
 			return con.connect(user="root",password="2591439",database="dedb")
-		except: return SyntaxException
-		
-
-
-#---------------------------------------------------------------------------------------------WEAPON
-class Weapom():
-	def getAll(self):
-		try:
-			db = DBCon.up()
-			crs = db.cursor(dictionary=True)
-			query="""SELECT * FROM weapon;"""
-			crs.execute(query)
-			results = crs.fetchall()
-			crs.close()
-			return results
-		except Exception: raise SyntaxException
-
-	def getByID(self, id:int):
-		try:
-			db = DBCon.up()
-			crs = db.cursor(dictionary=True)
-			crs.execute(f"""SELECT * FROM weapon where `id` = {id};""")
-			result = crs.fetchone()
-			crs.close()
-
-			if result == None : raise NotFoundException
-			return result
-		except NotFoundException: raise NotFoundException
-		except Exception: raise SyntaxException
-
-	def getByProp(self, prop:str):
-		try:
-			db = DBCon.up()
-			crs= db.cursor(dictionary=True)
-			crs.execute("select `type` from  weapon group by `type`;")
-			types=[x["type"] for x in crs.fetchall()]
-
-			if prop in types:
-				crs.execute(f"select * from  weapon WHERE `type`= '{prop.lower()}';")
-				results= crs.fetchall()
-				crs.close()
-			else: 
-				crs.execute(f"select * from  weapon WHERE `name`= '{prop.lower()}';")
-				results= crs.fetchone()
-				if results == None: raise NotFoundException
-				crs.close()
-			return results
-		except NotFoundException: raise NotFoundException
-		except Exception:raise SyntaxException
-
-	def new(self, data:dict):
-		try:
-			keys=""
-			values=""
-			for key in data.keys():
-				keys+="`%s`,"%key
-				match key:
-					case "name": values+="'%s',"%data[key].lower()
-					case "type": values+="'%s',"%data[key].lower()
-					case _: values+="%s,"%data[key]
-				
-			db = DBCon.up()
-			crs = db.cursor()
-			query="""INSERT INTO weapon(%s) VALUES(%s);""" %(keys[:-1],values[:-1])
-			crs.execute(query)
-			db.commit()
-			crs.close()
-		except Exception:raise SyntaxException
-	
-	def drop(self, id:int):
-		try:
-			Weapom().getByID(id)
-			db = DBCon.up()
-			crs = db.cursor()
-			crs.execute("""DELETE FROM weapon WHERE `id` = %s;"""%id)
-			db.commit()
-			crs.close()
-		except NotFoundException : raise NotFoundException
-		except Exception: raise SyntaxException
-		
-	def full_update(self, data:dict):
-		try:
-			Weapom().getByID(data["id"])
-			if len(data) != 6: raise SyntaxError
-			db = DBCon.up()
-			crs = db.cursor()
-			crs.execute(f"""UPDATE weapon SET 
-			   `type`='{data["type"].lower()}', 
-			   `name`='{data["name"].lower()}', 
-			   `atk`={data["atk"]},
-			   `atks`={data["atks"]},
-			   `def`={data["def"]}
-			   WHERE `id` = {data["id"]};""")
-			db.commit()
-			crs.close()
-		except NotFoundException: raise NotFoundException
-		except Exception:raise SyntaxException
-
-	def parcial_update(self, data:dict):
-		try:
-			Weapom().getByID(data["id"])
-			sets = ""
-			for key in data.keys():
-				sets+= f"`{key}`="
-				match key:
-					case "name": sets += f"'{data[key].lower()}',"
-					case "type": sets += f"'{data[key].lower()}',"
-					case _: sets += f"{data[key]},"
-
-			db = DBCon.up()
-			crs = db.cursor()
-			crs.execute(f"""UPDATE weapon SET {sets[:-1]} WHERE `id`={data["id"]} ;""")
-			db.commit()
-			crs.close()
-		except NotFoundException: raise NotFoundException
-		except Exception:raise SyntaxException
-
-
-#---------------------------------------------------------------------------------------------ARMOUR
-class Armour():
-	def getAll(self):
-		try:
-			db = DBCon.up()
-			crs = db.cursor(dictionary=True)
-			query="""SELECT * FROM armour;"""
-			crs.execute(query)
-			results = crs.fetchall()
-			crs.close()
-			return results
-		except: raise SyntaxException
-
-	def getByID(self, id:int):
-		try:
-			db = DBCon.up()
-			crs = db.cursor(dictionary=True)
-			crs.execute(f"""SELECT * FROM armour where `id` = {id};""")
-			result = crs.fetchone()
-			crs.close()
-
-			if result == None : raise NotFoundException
-			return result
-		except NotFoundException: raise NotFoundException
-		except Exception: raise SyntaxException
-
-	def getByProp(self, prop:str):
-		try:
-			db = DBCon.up()
-			crs= db.cursor(dictionary=True)
-			crs.execute("select `type` from  armour group by `type`;")
-			types=[x["type"] for x in crs.fetchall()]
-
-			if prop in types:
-				crs.execute(f"select * from  armour WHERE `type`= '{prop.lower()}';")
-				results= crs.fetchall()
-				crs.close()
-			else: 
-				crs.execute(f"select * from  armour WHERE `name`= '{prop.lower()}';")
-				results= crs.fetchone()
-				if results == None: raise NotFoundException
-				crs.close()
-			return results
-		except NotFoundException: raise NotFoundException
-		except Exception:raise SyntaxException
-
-	def new(self, data:dict):
-		try:
-			keys=""
-			values=""
-			for key in data.keys():
-				keys+="`%s`,"%key
-				match key:
-					case "name": values+="'%s',"%data[key].lower()
-					case "type": values+="'%s',"%data[key].lower()
-					case _: values+="%s,"%data[key]
-				
-			db = DBCon.up()
-			crs = db.cursor()
-			query="""INSERT INTO armour(%s) VALUES(%s);""" %(keys[:-1],values[:-1])
-			crs.execute(query)
-			db.commit()
-			crs.close()
-		except Exception:raise SyntaxException
-	
-	def drop(self, id:int):
-		try:
-			Armour().getByID(id)
-			db = DBCon.up()
-			crs = db.cursor()
-			crs.execute("""DELETE FROM armour WHERE `id` = %s;"""%id)
-			db.commit()
-			crs.close()
-		except NotFoundException : raise NotFoundException
-		except Exception: raise SyntaxException
-		
-	def full_update(self, data:dict):
-		try:
-			db = DBCon.up()
-			Armour().getByID(data["id"])
-			if len(data) != 6: raise SyntaxError
-			crs = db.cursor()
-			crs.execute(f"""UPDATE armour SET 
-			   `type`='{data["type"].lower()}', 
-			   `name`='{data["name"].lower()}', 
-			   `atk`={data["atk"]},
-			   `hp`={data["hp"]},
-			   `def`={data["def"]}
-			   WHERE `id` = {data["id"]};""")
-			db.commit()
-			crs.close()
-		except NotFoundException: raise NotFoundException
-		except Exception:raise SyntaxException
-
-	def parcial_update(self, data:dict):
-		try:
-			Armour().getByID(data["id"])
-			sets = ""
-			for key in data.keys():
-				sets+= f"`{key}`="
-				match key:
-					case "name": sets += f"'{data[key].lower()}',"
-					case "type": sets += f"'{data[key].lower()}',"
-					case _: sets += f"{data[key]},"
-
-			db = DBCon.up()
-			crs = db.cursor()
-			crs.execute(f"""UPDATE armour SET {sets[:-1]} WHERE `id`={data["id"]} ;""")
-			db.commit()
-			crs.close()
-		except NotFoundException: raise NotFoundException
-		except Exception:raise SyntaxException
-
-#---------------------------------------------------------------------------------------------TRINKET
-class Trinket():
-	def getAll(self):
-		try:
-			db = DBCon.up()
-			crs = db.cursor(dictionary=True)
-			query="""SELECT * FROM trinket;"""
-			crs.execute(query)
-			results = crs.fetchall()
-			crs.close()
-			return results
-		except: raise SyntaxException
-
-	def getByID(self, id:int):
-		try:
-			db = DBCon.up()
-			crs = db.cursor(dictionary=True)
-			crs.execute(f"""SELECT * FROM trinket where `id` = {id};""")
-			result = crs.fetchone()
-			crs.close()
-
-			if result == None : raise NotFoundException
-			return result
-		except NotFoundException: raise NotFoundException
-		except Exception: raise SyntaxException
-
-	def getByProp(self, prop:str):
-		try:
-			db = DBCon.up()
-			crs= db.cursor(dictionary=True)
-			crs.execute("select `type` from  trinket group by `type`;")
-			types=[x["type"] for x in crs.fetchall()]
-
-			if prop in types:
-				crs.execute(f"select * from  trinket WHERE `type`= '{prop.lower()}';")
-				results= crs.fetchall()
-				crs.close()
-			else: 
-				crs.execute(f"select * from  trinket WHERE `name`= '{prop.lower()}';")
-				results= crs.fetchone()
-				if results == None: raise NotFoundException
-				crs.close()
-			return results
-		except NotFoundException: raise NotFoundException
-		except Exception:raise SyntaxException
-
-	def new(self, data:dict):
-		try:
-			keys=""
-			values=""
-			for key in data.keys():
-				keys+="`%s`,"%key
-				match key:
-					case "name": values+="'%s',"%data[key].lower()
-					case "type": values+="'%s',"%data[key].lower()
-					case _: values+="%s,"%data[key]
-
-			db = DBCon.up()	
-			crs = db.cursor()
-			query="""INSERT INTO trinket(%s) VALUES(%s);""" %(keys[:-1],values[:-1])
-			crs.execute(query)
-			db.commit()
-			crs.close()
-		except Exception:raise SyntaxException
-	
-	def drop(self, id:int):
-		try:
-			Trinket().getByID(id)
-			db = DBCon.up()
-			crs = db.cursor()
-			crs.execute("""DELETE FROM trinket WHERE `id` = %s;"""%id)
-			db.commit()
-			crs.close()
-		except NotFoundException : raise NotFoundException
-		except Exception: raise SyntaxException
-		
-	def full_update(self, data:dict):
-		try:
-			Trinket().getByID(data["id"])
-			db = DBCon.up()
-			if len(data) != 6: raise SyntaxError
-			crs = db.cursor()
-			crs.execute(f"""UPDATE trinket SET 
-			   `type`='{data["type"].lower()}', 
-			   `name`='{data["name"].lower()}', 
-			   `atk`={data["atk"]},
-			   `hp`={data["hp"]},
-			   `def`={data["def"]}
-			   WHERE `id` = {data["id"]};""")
-			db.commit()
-			crs.close()
-		except NotFoundException: raise NotFoundException
-		except Exception:raise SyntaxException
-
-	def parcial_update(self, data:dict):
-		try:
-			db = DBCon.up()
-			Trinket().getByID(data["id"])
-			sets = ""
-			for key in data.keys():
-				sets+= f"`{key}`="
-				match key:
-					case "name": sets += f"'{data[key].lower()}',"
-					case "type": sets += f"'{data[key].lower()}',"
-					case _: sets += f"{data[key]},"
-
-			crs = db.cursor()
-			crs.execute(f"""UPDATE trinket SET {sets[:-1]} WHERE `id`={data["id"]} ;""")
-			db.commit()
-			crs.close()
-		except NotFoundException: raise NotFoundException
-		except Exception:raise SyntaxException
+		except: return DBConnectionFail	
 
 #---------------------------------------------------------------------------------------------ACCOUNT
 class Account():
-	def getAll(self):
+	def get_all(self):
 		try:
 			db = DBCon.up()
+
 			crs = db.cursor(dictionary=True)
-			query="""SELECT * FROM account;"""
+			query="""SELECT id,usuario,email FROM account;"""
 			crs.execute(query)
 			results = crs.fetchall()
 			crs.close()
 			return results
-		except: raise SyntaxException
+		except: raise DBConnectionFail
 
-	def getByID(self, id:int):
+	def get_by_id(self, id:int):
 		try:
 			db = DBCon.up()
+
 			crs = db.cursor(dictionary=True)
-			crs.execute(f"""SELECT * FROM account where `id` = {id};""")
+			crs.execute(f"""SELECT id,email,senha FROM account where `id` = {id};""")
 			result = crs.fetchone()
 			crs.close()
 
 			if result == None : raise NotFoundException
 			return result
+		except DBConnectionFail: raise DBConnectionFail
 		except NotFoundException: raise NotFoundException
-		except Exception: raise SyntaxException
 
 	def new(self, data:dict):
 		try:
-			#teste para ususario ja existente
 			db = DBCon.up()
+
 			crs = db.cursor(dictionary=True)
-			crs.execute(f"""SELECT * FROM account where `usuario` = "{data["usuario"]}";""")
+			crs.execute(f"""SELECT `usuario` FROM account where `usuario` = "{data["usuario"]}";""")
 			result = crs.fetchone()
 			crs.close()
 			if result != None : raise UserAlreadyExistException
@@ -395,24 +56,26 @@ class Account():
 			crs.execute(query)
 			db.commit()
 			crs.close()
+
+		except DBConnectionFail: raise DBConnectionFail	
 		except UserAlreadyExistException: raise UserAlreadyExistException
 		except Exception:raise SyntaxException
 	
 	def drop(self, id:int):
 		try:
-			Account().getByID(id)
+			Account().get_by_id(id)
 			db = DBCon.up()
 			crs = db.cursor()
 			crs.execute("""DELETE FROM account WHERE `id` = %s;"""%id)
 			db.commit()
 			crs.close()
 		except NotFoundException : raise NotFoundException
-		except Exception: raise SyntaxException
 		
-	def full_update(self, data:dict):
+	def update(self, data:dict):
 		try:
-			Account().getByID(data["id"])
+			Account().get_by_id(data["id"])
 			if len(data) != 4: raise SyntaxError
+
 			db = DBCon.up()
 			crs = db.cursor()
 			crs.execute(f"""UPDATE account SET 
@@ -422,23 +85,32 @@ class Account():
 			   WHERE `id` = {data["id"]};""")
 			db.commit()
 			crs.close()
+
+		except DBConnectionFail: raise DBConnectionFail
 		except NotFoundException: raise NotFoundException
 		except Exception:raise SyntaxException 
+
+
+
+
 
 #---------------------------------------------------------------------------------------------BUILD
 class Build():
 	def get_all(self):
 		try:
 			db = DBCon.up()
+
 			crs = db.cursor(dictionary=True)
 			query = """SELECT * FROM build;"""
 			crs.execute(query)
 			result = crs.fetchall()
 			crs.close()
 			return result
+		
+		except DBConnectionFail: raise DBConnectionFail
 		except: raise SyntaxException
 
-	def get_byID(self,id:int):
+	def get_by_id(self,id:int):
 		try:
 			db = DBCon.up()
 			crs = db.cursor(dictionary=True)
@@ -449,54 +121,69 @@ class Build():
 
 			if result == None: raise NotFoundException
 			return result
+		except DBConnectionFail: raise DBConnectionFail
 		except NotFoundException : raise NotFoundException
-		except Exception : raise SyntaxError
 
 	def new(self, data:dict):
-		print(data)
 		try:
 			db = DBCon.up()	
 			crs = db.cursor()
-			query = f"""INSERT INTO build(`name`, `helmet`, `body`, `gloves`, `boots`, `main_hand`, `off_hand`, `amulet`, `ring`, `belt`) VALUES(
-				'{data["name"]}', {data["helmet"]}, {data["body"]}, {data["gloves"]}, {data["boots"]}, {data["main_hand"]},
-				 {data["off_hand"]}, {data["amulet"]}, {data["ring"]}, {data["belt"]}
-				 );""" 
+			keys,values = kv_query(data)
+			query = f"""insert into build({keys}) values({values});"""
+			
 			crs.execute(query)
 			db.commit()
 			crs.close()
+		except DBConnectionFail: raise DBConnectionFail
 		except Exception:raise SyntaxException
 
 	def drop(self, id:int):
 		try:
-			Build().get_byID(id)
+			Build().get_by_id(id)
+
 			db = DBCon.up()
 			crs = db.cursor()
 			crs.execute("""DELETE FROM build WHERE `id` = %s;"""%id)
 			db.commit()
 			crs.close()
-		except NotFoundException : raise NotFoundException
-		except Exception: raise SyntaxException
 
-	def full_update(self, data:dict):
+		except DBConnectionFail: raise DBConnectionFail
+		except NotFoundException : raise NotFoundException
+
+	def update(self, data:dict):
 		try:
-			Build().get_byID(data["id"])
-			if len(data) != 11: raise SyntaxError
+			Build().get_by_id(data["id"])
+			if len(data) != 12: raise SyntaxError
 			db = DBCon.up()
 			crs = db.cursor()
 			query = f"""UPDATE build SET 
-			   `name`= '{data["name"]}', 
-			   `helmet`= {data["helmet"]}, 
-			   `body`= {data["body"]},
-			   `gloves`= {data["gloves"]},
-			   `boots`= {data["boots"]},
-			   `main_hand`= {data["main_hand"]},
-			   `off_hand`= {data["off_hand"]},
-			   `amulet`= {data["amulet"]},
-			   `ring`= {data["ring"]},
-			   `belt`= {data["belt"]}
-			   WHERE `id` = {data["id"]};"""
+				`id_account`= {data["id_account"]},
+				`name`= '{data["name"]}', 
+				`helmet`= {data["helmet"]}, 
+				`body`= {data["body"]},
+				`gloves`= {data["gloves"]},
+				`boots`= {data["boots"]},
+				`main_hand`= {data["main_hand"]},
+				`off_hand`= {data["off_hand"]},
+				`amulet`= {data["amulet"]},
+				`ring`= {data["ring"]},
+				`belt`= {data["belt"]}
+				WHERE `id` = {data["id"]};"""
 			crs.execute(query)
 			db.commit()
 			crs.close()
+
 		except NotFoundException: raise NotFoundException
 		except Exception:raise SyntaxException 
+
+
+
+def kv_query(data:dict):
+	keys =""
+	values=""
+	for item in data.keys():
+		keys+= f"{item},"
+		values+= f"'{data[item]}'," if type(data[item]) is str else f"{data[item]}," if data[item] != None else "null,"
+	keys=keys[0:-1]
+	values=values[0:-1]
+	return keys,values
